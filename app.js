03f6d7119e36a4c085937c4156119315518d7d82
Dmitriy Meshechkov <d.meshechkov@yandex.ru>
2022-08-23 23:55:44 +0500

    application Overweight with If&Else

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 //application Overweight
 
-const myWeight = prompt('Enter your weight');
-const myHeight = prompt('Enter your height');
+// const myWeight = prompt('Enter your weight');
+// const myHeight = prompt('Enter your height');
+// const bodyMassIndex = myWeight / Math.pow(myHeight, 2);
+// const areYouOverweight = bodyMassIndex >= 25;
+// alert('Are you overweight?' + ' ' + areYouOverweight);
+
+
+
+//application Overweight with If&Else
+
+const myWeight = prompt('Enter your weight (kg)');
+const myHeight = prompt('Enter your height (m)');
 const bodyMassIndex = myWeight / Math.pow(myHeight, 2);
 const areYouOverweight = bodyMassIndex >= 25;
-alert('Are you overweight?' + ' ' + areYouOverweight);
+
+
+if (areYouOverweight) {
+    alert(`Your overweight is too much ${bodyMassIndex - 25}`);
+    console.log(`Your overweight is too much ${bodyMassIndex}`);
+} else {
+    alert(`Your overweight is norm ${bodyMassIndex}`);
+    console.log(`Your overweight is norm ${bodyMassIndex - 25}`);
+}
