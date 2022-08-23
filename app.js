fe080c3d122dd216152876f0d7a0083152e695ee
Dmitriy Meshechkov <d.meshechkov@yandex.ru>
2022-08-23 22:26:45 +0500

    Made application Overweight

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-alert('Hello again');
+const myWeight = prompt('Enter your weight');
+const myHeight = prompt('Enter your height');
+const bodyMassIndex = myWeight / Math.pow(myHeight, 2);
+const areYouOverweight = bodyMassIndex >= 25;
+alert('Are you overweight?' + ' ' + areYouOverweight);
