b8030117f4ee7d97a106d8046abba334ade46e2c
Dmitriy Meshechkov <d.meshechkov@yandex.ru>
2022-08-26 11:50:51 +0500

    Application Sales Departments Task

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-//application Overweight
+'use strict';
+
+// Application Overweight
 
 // const myWeight = prompt('Enter your weight');
 // const myHeight = prompt('Enter your height');
@@ -8,18 +10,78 @@
 
 
 
-//application Overweight with If&Else
+// Application Overweight with If&Else
 
-const myWeight = prompt('Enter your weight (kg)');
-const myHeight = prompt('Enter your height (m)');
-const bodyMassIndex = myWeight / Math.pow(myHeight, 2);
-const areYouOverweight = bodyMassIndex >= 25;
+// const myWeight = prompt('Enter your weight (kg)');
+// const myHeight = prompt('Enter your height (m)');
+// const bodyMassIndex = myWeight / Math.pow(myHeight, 2);
+// const areYouOverweight = bodyMassIndex >= 25;
 
 
-if (areYouOverweight) {
-    alert(`Your overweight is too much ${bodyMassIndex - 25}`);
-    console.log(`Your overweight is too much ${bodyMassIndex}`);
-} else {
-    alert(`Your overweight is norm ${bodyMassIndex}`);
-    console.log(`Your overweight is norm ${bodyMassIndex - 25}`);
-}
+// if (areYouOverweight) {
+//     alert(`Your overweight is too much ${bodyMassIndex - 25}`);
+//     console.log(`Your overweight is too much ${bodyMassIndex}`);
+// } else {
+//     alert(`Your overweight is norm ${bodyMassIndex}`);
+//     console.log(`Your overweight is norm ${bodyMassIndex - 25}`);
+// }
+
+
+// Application Ternary operator
+
+// const age = prompt('Enter your age');
+// const message = age >= 18 ? 'You can buy it' : 'You can not buy it';
+// console.log(message);
+// alert(message);
+
+
+    // Application Sales Departments Task
+
+// function getAverage(month1, month2, month3) {
+//     return (month1 + month2 + month3) / 3;
+//     }
+
+
+// // 1 quater
+
+// const dept1AverSales1 = getAverage(35467, 29842, 38501);
+// const dept2AverSales1 = getAverage(70533, 50121, 33899);
+// console.log(dept1AverSales1, dept2AverSales1);
+ 
+// // 2 quater
+
+// const dept1AverSales2 = getAverage(50301, 121984, 19207);
+// const dept2AverSales2 = getAverage(72381, 41562, 29465);
+// console.log(dept1AverSales2, dept2AverSales2);
+
+
+// const printBonus = function(dept1AverSales, dept2AverSales) {
+//     if(dept1AverSales > dept2AverSales) {
+//         const diffAverSales = dept1AverSales - dept2AverSales;
+//         const persBonus = diffAverSales / dept2AverSales * 100;
+//         if(persBonus >= 30) {
+//             console.log(`Dept 1 will get a bonus ${persBonus}%`);
+//         } else {
+//             console.log('No bonus');
+//         }
+//     } else if(dept2AverSales > dept1AverSales) {
+//             const diffAverSales = dept2AverSales - dept1AverSales;
+//             const persBonus = diffAverSales / dept1AverSales * 100;
+//             if(persBonus >= 30) {
+//                 console.log(`Dept 2 will get a bonus ${persBonus}%`);
+//             } else {
+//                 console.log('No bonus');
+//             } 
+//     } else {
+//         console.log('No bonus');
+//     }
+// }
+
+// printBonus(dept1AverSales1, dept2AverSales1);
+// printBonus(dept1AverSales2, dept2AverSales2);
+
+
+
+
+
+
